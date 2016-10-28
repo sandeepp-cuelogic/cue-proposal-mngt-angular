680acf006d8df18b1cc24102cca5a44bf89e659b
Priti Gupta <priti.gupta@cuelogic.co.in>
2016-10-28 17:37:36 +0530

    change

--- app/modules/auth/controllers/login.js
+++ app/modules/auth/controllers/login.js
@@ -11,18 +11,22 @@
         $scope.error = '';
         
         if($state.current.name == "base.logout"){
-           localStorageServiceWrapper.set('token','');
+           localStorageServiceWrapper.set('current_user',{});
         }
        $scope.LoggedIn = function(){
          if($scope.user.email && $scope.user.password){
             loginService.validate($scope.user.email,$scope.user.password)
             .success(function (data, status, headers, config) {
-                 //console.log(data) ;
+                 console.log(data) ;
                  if(data.statusCode == 400) {
                    $scope.error = data.message;
                  }
                  else{
-                    localStorageServiceWrapper.set('token',data.token);
+                    var current_user_info = {};
+                    current_user_info.token = data.data.token;
+                    current_user_info.uname = data.data.user.name;
+                    //console.log(current_user_info) ;
+                    localStorageServiceWrapper.set('current_user',current_user_info);
                     $rootScope.message = data.message;
                     $state.go('base.proposal');
                  }
